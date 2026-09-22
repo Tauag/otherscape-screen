c51471fd398d79be2ca013090bfb238a3f1dfb10
Gavin Li <dev.gavin.li@gmail.com>
2026-09-22 14:39:10 -0400

    prevent scrolling of the entire window

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
 	return (
 		<html
 			lang="en"
-			className={`${chakraPetch.variable} ${barlow.variable} ${jetbrainsMono.variable} h-full antialiased`}
+			className={`${chakraPetch.variable} ${barlow.variable} ${jetbrainsMono.variable} h-full antialiased overflow-hidden`}
 		>
 			<body className="min-h-full flex flex-col">{children}</body>
 		</html>
